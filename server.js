ee4f8e4deb07cbc1e36e3b910f1d62bce1c1183f
Hunter Lacefield <lace1010@gmail.com>
2021-02-21 20:18:08 +0800

    Change unix if statement to handle year having more than 4 digits as well

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ app.get("/api/timestamp/:date", function (req, res) {
   let dateInArray = date_string.split("-");
 
   // Create a js date if it is passed in year-month-day format
-  if (dateInArray.length > 1) {
+  // if the first item in the array is greater than 4, handles unix as the first item is only item and it will have more than 4 numbers.
+  // Also handles if the year is 5 digits or more
+  if (dateInArray[0] > 4) {
     date = new Date(dateInArray[0], dateInArray[1] - 1, dateInArray[2]);
   }
   // Create a js date if it is passed in unix format
